22dbfe2eb8f802482ab3e4c18e45a55d60b4e146
Egor Svirin <Egor3Game@mail.ru>
2024-04-04 21:35:25 +0300

    Init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.financetracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.financetracker"
