47f77c5ac60cfbfdf874276a57130627e59dd782
Egor Svirin <Egor3Game@mail.ru>
2024-04-25 03:26:47 +0300

    Create an adapter for transactions
    1. Create 2 adapters for visualize transactions
    2. Small changes in design

---
+++ app/src/main/java/com/example/financetracker/ui/adapters/CardTransactionAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.financetracker.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.financetracker.R
+import com.example.financetracker.data.Transaction
+
+class CardTransactionAdapter(private val transactions: List<Transaction>, private val context: Context) : RecyclerView.Adapter<CashTransactionAdapter.TransactionViewHolder>() {
+    private val cardTransactions = mutableListOf<Transaction>()
+
+    init {
+        splitTransactions()
+    }
+
+    private fun splitTransactions() {
+        cardTransactions.clear()
+
+        for (transaction in transactions) {
+            if (transaction.isCreditCard) {
+                cardTransactions.add(transaction)
+            }
+        }
+    }
+
+    class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageCategory: ImageView = itemView.findViewById(R.id.imageCategory)
+        val textDate: TextView = itemView.findViewById(R.id.textDate)
+        val textName: TextView = itemView.findViewById(R.id.textName)
+        val textAmount: TextView = itemView.findViewById(R.id.textAmount)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CashTransactionAdapter.TransactionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.transaction_item, parent, false)
+        return CashTransactionAdapter.TransactionViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return if(cardTransactions.size >= 4)
+            4
+        else
+            cardTransactions.size
+    }
+
+    override fun onBindViewHolder(holder: CashTransactionAdapter.TransactionViewHolder, position: Int) {
+        val reversedPosition = cardTransactions.size - 1 - position
+        val transaction = cardTransactions[reversedPosition]
+        holder.textName.text = transaction.transactionName
+        holder.textAmount.text = "${transaction.amount} RUB"
+        if (transaction.isIncome) {
+            holder.textAmount.setTextColor(ContextCompat.getColor(context, R.color.income))
+        } else {
+            holder.textAmount.setTextColor(ContextCompat.getColor(context, R.color.outcome))
+        }
+
+        val iconResId = when (transaction.category) {
+            "imageCar" -> R.drawable.icon_car_circle
+            "imageFood" -> R.drawable.icon_food_circle
+            "imageGift" -> R.drawable.icon_gift_circle
+            "imageHealth" -> R.drawable.icon_health_circle
+            "imageClothes" -> R.drawable.icon_clothes_circle
+            "imageDonation" -> R.drawable.icon_donation_circle
+            "imageBeauty" -> R.drawable.icon_beauty_cicle
+            "imageHome" -> R.drawable.icon_home_circle
+            else -> R.drawable.avatar_svgrepo_com
+        }
+        holder.imageCategory.setImageResource(iconResId)
+    }
+}
--- app/src/main/java/com/example/financetracker/ui/fragments/TransactionListFragment.kt
+++ app/src/main/java/com/example/financetracker/ui/fragments/TransactionListFragment.kt
@@ -1,24 +1,39 @@
 package com.example.financetracker.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.financetracker.R
 import com.example.financetracker.databinding.FragmentTransactionListBinding
+import com.example.financetracker.ui.adapters.CardTransactionAdapter
+import com.example.financetracker.ui.adapters.CashTransactionAdapter
+import com.example.financetracker.viewmodel.TransactionViewModel
 
-private lateinit var binding: FragmentTransactionListBinding
 class TransactionListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentTransactionListBinding
+    private lateinit var viewModel: TransactionViewModel
+    private lateinit var cashTransactionAdapter: CashTransactionAdapter
+    private lateinit var cardTransactionAdapter: CardTransactionAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTransactionListBinding.inflate(layoutInflater, container, false)
+
+        viewModel = ViewModelProvider(requireActivity()).get(TransactionViewModel::class.java)
+        cashTransactionAdapter = CashTransactionAdapter(viewModel.transactionsList, requireContext())
+        binding.recyclerCash.layoutManager = LinearLayoutManager(context)
+        binding.recyclerCash.adapter = cashTransactionAdapter
+
+        cardTransactionAdapter = CardTransactionAdapter(viewModel.transactionsList, requireContext())
+        binding.recyclerCard.layoutManager = LinearLayoutManager(context)
+        binding.recyclerCard.adapter = cardTransactionAdapter
+
         return binding.root
     }
 }
